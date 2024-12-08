4421e54d7be4a3be2d1ccf1b7f95470a952f3584
hyunzsu <hyunzsu@naver.com>
2024-12-08 13:45:25 +0900

    style: ButtonPanel 컴포넌트 스타일링
    - ButtonPanel에 Align, Add 버튼 그룹 추가
    - 공통 Button 컴포넌트 적용

--- src/components/Panel/ButtonPanel.tsx
+++ src/components/Panel/ButtonPanel.tsx
@@ -1,7 +1,28 @@
+import Button from '../@common/Button';
+
 export default function ButtonPanel() {
   return (
-    <section className="h-1/3 border-2 border-blue-400 p-2">
-      ButtonPanel
+    <section className="min-h-80 shrink-0 p-2.5">
+      {/* Align 그룹 */}
+      <section className="mb-2 bg-gray-300 p-2">
+        <h3 className="mb-2 text-center font-semibold">Align</h3>
+        <div className="space-y-2">
+          <Button>All Vertically</Button>
+          <Button>All Horizontally</Button>
+          <Button>Group Vertically</Button>
+          <Button>Group Horizontally</Button>
+        </div>
+      </section>
+
+      {/* Add 그룹 */}
+      <section className="bg-gray-300 p-2">
+        <h3 className="mb-2 text-center font-semibold">Add</h3>
+        <div className="space-y-2">
+          <Button>Div</Button>
+          <Button>Span</Button>
+          <Button>Paragraph</Button>
+        </div>
+      </section>
     </section>
   );
 }
--- src/components/Panel/LayerPanel.tsx
+++ src/components/Panel/LayerPanel.tsx
@@ -1,6 +1,6 @@
 export default function LayerPanel() {
   return (
-    <section className="h-2/3 border-2 border-green-400 p-2">
+    <section className="flex-1 border-2 border-green-400 p-2">
       LayerPanel
     </section>
   );
