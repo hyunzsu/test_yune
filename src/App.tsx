524d5e7ad42d5a3def1bde41dfeff058e6bd8e8e
hyunzsu <hyunzsu@naver.com>
2024-12-09 01:42:06 +0900

    style: 레이아웃 정리

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Panel from './components/Panel';
 import Viewport from './components/Viewport';
 function App() {
   return (
-    <main className="flex h-screen w-full">
+    <main className="flex min-h-screen w-full">
       <Panel />
       <Viewport />
     </main>
--- src/components/Panel/index.tsx
+++ src/components/Panel/index.tsx
@@ -3,7 +3,7 @@ import LayerPanel from './LayerPanel';
 
 export default function Panel() {
   return (
-    <aside className="flex h-full w-1/6 flex-col bg-zinc-600">
+    <aside className="flex w-1/6 flex-col bg-zinc-600">
       <ButtonPanel />
       <LayerPanel />
     </aside>
