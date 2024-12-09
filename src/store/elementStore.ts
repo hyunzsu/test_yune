7e6e1af739c633184157321ed9b01022fe761270
hyunzsu <hyunzsu@naver.com>
2024-12-09 15:29:46 +0900

    feat: 요소 선택 기능 추가
    - elementStore에 선택 관련 상태와 액션 추가
    - selectedId: 선택된 요소의 id 저장
    - selectElement: 요소 선택 액션 추가

--- src/store/elementStore.ts
+++ src/store/elementStore.ts
@@ -3,11 +3,14 @@ import { Element, ElementType } from '../types/element';
 
 interface ElementStore {
   elements: Element[];
+  selectedId: string | null;
   addElement: (type: ElementType) => void;
+  selectElement: (id: string) => void;
 }
 
 export const useElementStore = create<ElementStore>((set) => ({
   elements: [],
+  selectedId: null,
   addElement: (type) => {
     set((state) => {
       const newElement: Element = {
@@ -19,10 +22,12 @@ export const useElementStore = create<ElementStore>((set) => ({
           height: 190,
         },
       };
-
       return {
         elements: [...state.elements, newElement],
       };
     });
+  },
+  selectElement: (id) => {
+    set({ selectedId: id });
   },
 }));
