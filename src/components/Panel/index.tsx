7dd12179a5db0f3887dbb2cd22dfd9d8e40033c9
hyunzsu <hyunzsu@naver.com>
2024-12-10 22:15:46 +0900

    fix: Panel 컴포넌트 너비 고정
    - shrink-0 속성 추가로 Panel이 화면 크기에 따라 축소되는 현상 방지

--- src/components/Panel/index.tsx
+++ src/components/Panel/index.tsx
@@ -3,7 +3,7 @@ import LayerPanel from './LayerPanel';
 
 export default function Panel() {
   return (
-    <aside className="flex w-1/6 flex-col bg-zinc-600">
+    <aside className="flex w-1/6 shrink-0 flex-col bg-zinc-600">
       <ButtonPanel />
       <LayerPanel />
     </aside>
